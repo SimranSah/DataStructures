0edb6f9496f0ed7617fa6c27f9a25f46d61243fe
SimranSah <52495997+SimranSah@users.noreply.github.com>
2019-12-14 11:17:55 +0530

    Add files via upload

---
+++ FUNC.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void italy();
+void argentina();
+void brazilian();
+void indian();
+void main()
+{
+ printf("I AM IN MAIN ");
+ italy();
+ argentina();
+ brazilian();
+ indian();
+ return 0;
+ }
+void italy()
+{
+ printf("I AM IN ITALY ");
+ }
+void argentina()
+{
+ printf("I AM IN ARGENTINA");
+ }
+void brazilian()
+{
+ printf("I AM IN BRAZIL");
+ }
